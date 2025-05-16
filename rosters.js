f12f40978a0ae2b9353262d4d6a40f581217f39c
526cloiacono <526cloiacono@frhsd.com>
2025-05-16 13:07:18 -0400

    added modal with the help of dan

--- rosters.js
+++ rosters.js
@@ -17,10 +17,30 @@ col.innerHTML = `
             <h5 class="card-title mb-1"> ${p.name} </h5>
             <div class="badge badge-position badge-pos-${p.role}">${p.role}</div>
             <p class="small text-muted mb-0">Skills:<br>${p.skills}</p>
-             <button class="btn btn-primary mt-2 btn-task">Show Task</button>
+             <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+Click to see there task!
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">This is what ${p.name} would do if they fought the gorrlia</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      ${p.task}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
         </div>
     </div> 
-
 `
 
 grid.appendChild(col)
